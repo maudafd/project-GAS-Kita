2e4403f52a85f9efda359299347932e0552b519f
Rama <rambokece03@gmail.com>
2025-11-26 10:33:25 +0700

    membenarkan navigasi bar dan menambahkan homeFragment, riwayatFragment, dan profileFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.cardview)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
