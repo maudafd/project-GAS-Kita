2156f887edf96bf8b1bace6a42b7364c3c0cc97d
FAIZ FATHIR <faizfathir1002@gmail.com>
2025-12-06 16:15:57 +0700

    memindahkan fragment pembayaran ke dalam package Order dan menambahkan recycler view di pembayarancashfragment.kt

--- app/src/main/java/com/example/gaskita/Order/PembayaranQrisFragment.kt
+++ app/src/main/java/com/example/gaskita/Order/PembayaranQrisFragment.kt
@@ -1,4 +1,4 @@
-package com.example.gaskita.Dashboard
+package com.example.gaskita.Order
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.gaskita.R
 
-class PembayaranCashFragment : Fragment() {
+class PembayaranQrisFragment : Fragment() {
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pembayaran_cash, container, false)
+
+        return inflater.inflate(R.layout.fragment_pembayaran_qris, container, false)
     }
+
 
 }
